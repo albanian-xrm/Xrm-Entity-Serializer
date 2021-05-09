3ff69f0088d72a76bb248fb3e49290101c8f807e
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2021-05-09 16:17:12 +0200

    Improved coverage.
    Added coverage-report.ps1

---
+++ XrmEntitySerializer.Tests/ParameterCollectionTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xrm.Sdk;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace XrmEntitySerializer.Tests
+{
+    public class ParameterCollectionTests
+    {
+        [Fact]
+        public void ParameterCollectionCanBeSerializedAndDeserialized()
+        {
+            ParameterCollection parameterCollection = new ParameterCollection();
+            parameterCollection.Add("Entity", new Entity("account"));
+            parameterCollection.Add("EntityReference", new EntityReference("contact", Guid.NewGuid()));
+
+            EntitySerializer serializer = new EntitySerializer();
+            serializer.TypeNameHandling = TypeNameHandling.Objects;
+            MemoryStream memoryStream = new MemoryStream(new byte[9000], true);
+
+            using (StreamWriter writer = new StreamWriter(memoryStream))
+            {
+                serializer.Serialize(new JsonTextWriter(writer), parameterCollection);
+            }
+
+            ParameterCollection deserializedParameterCollection;
+            memoryStream = new MemoryStream(memoryStream.ToArray());
+            using (StreamReader reader = new StreamReader(memoryStream))
+            {
+                deserializedParameterCollection = (ParameterCollection)serializer.Deserialize(new JsonTextReader(reader), typeof(ParameterCollection));
+            }
+
+            Assert.Equal(parameterCollection.GetType(), deserializedParameterCollection.GetType());
+            Assert.Equal(parameterCollection.Count, deserializedParameterCollection.Count);
+            Assert.Equal(parameterCollection.Keys.First(), deserializedParameterCollection.Keys.First());
+        }
+    }
+}
