dc04c76e3ae353f585d8751a78fccb724eca99aa
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2022-06-28 20:41:18 +0200

    added new tests

---
+++ XrmEntitySerializer.Tests/ParameterCollectionTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xrm.Sdk;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using Xunit;
+
+namespace XrmEntitySerializer.Tests
+{
+    public class ParameterCollectionTests
+    {
+        [Fact]
+        public void ParameterCollectionCanBeSerializedAndDeserialized()
+        {
+            RemoteExecutionContext executionContext = new RemoteExecutionContext();
+            executionContext.InputParameters.Add("Target", new Entity("account") { Id = Guid.NewGuid() });
+
+
+            EntitySerializer serializer = new EntitySerializer();
+            MemoryStream memoryStream = new MemoryStream(new byte[9000], true);
+
+            using (StreamWriter writer = new StreamWriter(memoryStream))
+            {
+                serializer.Serialize(new JsonTextWriter(writer), executionContext);
+            }
+
+            RemoteExecutionContext deserializedExecutionContext;
+            memoryStream = new MemoryStream(memoryStream.ToArray());
+            using (StreamReader reader = new StreamReader(memoryStream))
+            {
+                deserializedExecutionContext = (RemoteExecutionContext)serializer.Deserialize(new JsonTextReader(reader));
+            }
+
+            Assert.Equal(((Entity)executionContext.InputParameters["Target"]).LogicalName, ((Entity)deserializedExecutionContext.InputParameters["Target"]).LogicalName);
+        }
+    }
+}
